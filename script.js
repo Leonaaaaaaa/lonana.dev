ffcd381db0955f2a3f9c3b672ef7405f0d8a79a5
Lona <lonazwart@gmail.com>
2025-03-22 17:03:06 +0100

    clean slate, added font

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const birthYear = 2007;
-const birthMonth = 2;
-const birthDay = 24;
+setInterval(() => {
+    location.reload();
+}, 2000);
+function set_age(birthYear = 2007, birthMonth = 2, birthDay = 24) {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    const currentDay = today.getDate();
 
-const today = new Date();
-const currentYear = today.getFullYear();
-const currentMonth = today.getMonth() + 1;
-const currentDay = today.getDate();
+    let age = currentYear - birthYear;
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+        age--;
+    }
 
-let age = currentYear - birthYear;
-if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
-    age--;
+    return age
 }
 
-document.getElementById("age").textContent = age;
